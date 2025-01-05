09b6e98bd85a8bda3393148e2873865de254362d
Dens777 <dens77712@gmail.com>
2025-01-06 00:09:14 +0300

    Add files via upload

---
+++ module_6_1_corr.py
@@ -0,0 +1,59 @@
+class Animal:
+    alive = True  # живой или не живой
+    fed = False  # голодный или сытый
+
+    def __init__(self, name):
+        self.name = name
+
+
+    def eat(self, food): # Результат поедания чего ли-бо
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+class Plant:
+    def __init__(self, name):
+        self.name = name
+        self.edible = False  # съедобное или не съедобное
+
+
+# Определение классов-наследников для Animal
+class Mammal(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Predator(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+# Определение классов-наследников для Plant
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = False
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+# Создание объектов
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+# Вывод информации и тестирование методов
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
